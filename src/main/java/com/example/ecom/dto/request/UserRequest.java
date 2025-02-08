c7f06756c793d028e86f5d5271a11dc122a77c12
mishrasai1206 <mishrasai1206@gmail.com>
2025-02-08 20:50:03 +0530

    Updating DTOs with Lombok annotations

--- src/main/java/com/example/ecom/dto/request/UserRequest.java
+++ src/main/java/com/example/ecom/dto/request/UserRequest.java
@@ -1,5 +1,12 @@
 package com.example.ecom.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserRequest {
     private String username;
     private String email;
--- src/main/java/com/example/ecom/dto/response/UserResponse.java
+++ src/main/java/com/example/ecom/dto/response/UserResponse.java
@@ -1,9 +1,15 @@
 package com.example.ecom.dto.response;
 
 import com.example.ecom.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor
+@Builder
 public class UserResponse {
     private String id;
     private String username;
